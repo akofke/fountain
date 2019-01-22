83beeaa974d3ff40252a125c0e8ded5d49ad52f2
Alex Kofke <akofke@gmail.com>
2019-01-22 16:16:09 -0500

    ppm writer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
-use std::mem::size_of;
+use std::fs::File;
+use std::path::Path;
 
 mod math;
 
-use math::{Vector3, Vector3f};
+use math::{Vector3f};
+use std::io::BufWriter;
+use std::io::Write;
 
 fn main() {
-    let v1 = Vector3::new(1.0, 2.0, 3.0);
-    let mut v2 = Vector3f::new(2.0, 3.0, 4.0);
-    v2 += 3.0;
-    dbg!(v2);
+    let width = 1024;
+    let height = 768;
+    let mut framebuf: Vec<Vector3f> = Vec::with_capacity(width * height);
+    for j in 0..height {
+        for i in 0..width {
+            framebuf.push(Vector3f::new(j as f32 / height as f32, i as f32 / width as f32, 0.0));
+        }
+    }
+
+    write_ppm(width, height, &framebuf, "test.ppm").expect("Failed to write file");
+}
+
+
+fn write_ppm<P: AsRef<Path>>(width: usize, height: usize, framebuffer: &[Vector3f], path: P) -> std::io::Result<()> {
+    assert_eq!(framebuffer.len(), width * height);
+
+    let mut f = BufWriter::new(File::create(path)?);
+
+    write!(&mut f, "P6\n{} {}\n255\n", width, height)?;
+
+    for v in framebuffer.iter() {
+        f.write_all(&v.to_rgb())?;
+    }
+
+    Ok(())
 }
--- src/math.rs
+++ src/math.rs
@@ -1,36 +1,54 @@
+use num::clamp;
 use num::traits::{Float};
 use std::ops::AddAssign;
+use num::ToPrimitive;
 
 pub type Vector3f = Vector3<f32>;
 
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
-pub struct Vector3<T> where T: Float {
+pub struct Vector3<T>
+where
+    T: Float,
+{
     pub x: T,
     pub y: T,
-    pub z: T
+    pub z: T,
 }
 
 impl<T: Float> Vector3<T> {
     pub fn new(x: T, y: T, z: T) -> Self {
-        Self {
-            x,
-            y,
-            z
-        }
+        Self { x, y, z }
+    }
+
+    pub fn to_rgb(&self) -> [u8; 3] where T: ToPrimitive {
+        let zero = T::zero();
+        let one = T::one();
+        let scale = T::from(255).unwrap();
+        [
+            (clamp::<T>(self.x, zero, one) * scale).to_u8().unwrap(),
+            (clamp::<T>(self.y, zero, one) * scale).to_u8().unwrap(),
+            (clamp::<T>(self.z, zero, one) * scale).to_u8().unwrap(),
+        ]
     }
 }
 
 impl<T: Float> AddAssign<T> for Vector3<T> {
-
     fn add_assign(&mut self, other: T) {
-        *self = Vector3 {x: self.x + other, y: self.y + other, z: self.z + other};
+        *self = Vector3 {
+            x: self.x + other,
+            y: self.y + other,
+            z: self.z + other,
+        };
     }
 }
 
 impl<T: Float> AddAssign for Vector3<T> {
     fn add_assign(&mut self, rhs: Self) {
-        *self = Vector3 {x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z};
+        *self = Vector3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        };
     }
 }
-
