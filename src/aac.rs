fb812a10fa6d88c8fcf8a1e95d4eca8a25c9c58b
Alex Kofke <akofke@gmail.com>
2019-02-19 10:39:44 -0500

    More aac

--- src/aac.rs
+++ src/aac.rs
@@ -5,7 +5,7 @@ use crate::scene::PrimRef;
 use crate::morton::morton3;
 
 struct Aac {
-
+    morton_codes: Vec<u32>,
 }
 
 enum Cluster {
@@ -19,17 +19,23 @@ enum Cluster {
 impl Aac {
     pub fn build(primitives: &mut [PrimRef]) -> Self {
 
-        unimplemented!()
-    }
-
-    fn sort_primitives(primitives: &mut [PrimRef]) {
         // find scene bounding box
         let scene_bb = primitives.iter().fold(Aabb::empty(), |scene_bb, prim| scene_bb.join(&prim.aabb));
 
-        // TODO: radix sort!!
-        primitives.sort_by_cached_key(|prim| {
+        let mut morton_codes = primitives.iter().map(|prim| {
             let centroid = prim.aabb.normalized_by(&scene_bb).centroid();
             morton3(centroid.x, centroid.y, centroid.z)
-        })
+        }).collect::<Vec<_>>();
+
+        Aac::sort_primitives(primitives, &mut morton_codes);
+
+        unimplemented!()
     }
+
+    fn sort_primitives(primitives: &mut [PrimRef], morton_codes: &mut [u32]) {
+        // TODO: radix sort!!
+        primitives.sort_unstable_by_key(|prim| morton_codes[prim.idx]);
+        morton_codes.sort_unstable();
+    }
+
 }
