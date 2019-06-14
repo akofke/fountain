af426eb56834fc9237c48cb1de4ddd7bfc63fcdf
Alex Kofke <akofke@gmail.com>
2019-06-14 01:36:56 -0400

    Try more with Spectrum, still problems with const generics

--- src/spectrum.rs
+++ src/spectrum.rs
@@ -1,30 +1,42 @@
-//TODO: uncomment when const generics don't crash
 
-//
-//use crate::Float;
-//use std::ops::{Add, Sub, AddAssign, SubAssign, Mul, MulAssign, Div, DivAssign};
-//
-//pub trait SpectrumArith: Add + AddAssign + Sub + SubAssign + Mul + MulAssign + Div + DivAssign + Mul<Float>
-//+ MulAssign<Float> + Div<Float> + DivAssign<Float>
-//    where Self: Sized {}
-//
-//
-//#[derive(Clone, Copy)]
-//pub struct CoefficientSpectrum<const N: usize>
-//{
-//    pub c: [Float; N]
-//}
-//
-//impl<const N: usize> Add for CoefficientSpectrum<{N}> {
-//    type Output = Self;
-//
-//    fn add(self, rhs: Self) -> Self::Output {
-//        let mut c = [0.0 as Float; N];
-//        for i in 0..N {
-//            c[i] = self.c[i] + rhs.c[i];
-//        }
-//        CoefficientSpectrum { c }
-//    }
-//}
-//
 
+use crate::Float;
+use std::ops::{Add, Sub, AddAssign, SubAssign, Mul, MulAssign, Div, DivAssign, Index};
+
+pub trait SpectrumArith: Add + AddAssign + Sub + SubAssign + Mul + MulAssign + Div + DivAssign + Mul<Float>
++ MulAssign<Float> + Div<Float> + DivAssign<Float>
+    where Self: Sized {}
+
+pub trait CoefficientSpectrum<const N: usize>: Index<usize>  {
+
+}
+
+impl<S: CoefficientSpectrum<{N}>, const N: usize> Add for S {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        unimplemented!()
+    }
+}
+
+pub struct RGBSpectrum {
+    c: [Float; 3]
+}
+
+impl RGBSpectrum {
+    pub fn new(v: Float) -> Self {
+        Self {c: [v; 3]}
+    }
+}
+
+impl CoefficientSpectrum<{3}> for RGBSpectrum {
+
+}
+
+impl Index<usize> for RGBSpectrum {
+    type Output = Float;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.c[index]
+    }
+}
