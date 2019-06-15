c5b1781da9f7da923a393dc139d4af571e7fddf4
Alex Kofke <akofke@gmail.com>
2019-06-14 20:31:45 -0400

    Remove more of old code

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,11 @@
 
 #[macro_use] pub mod macros; // must stay at the top
 pub mod geom;
-pub mod camera;
 pub mod material;
 pub mod math;
 pub mod fast_rand;
 pub mod image;
 pub mod scene;
-pub mod renderer_old;
 pub mod aabb;
 pub mod bvh;
 pub mod morton;
@@ -115,50 +113,4 @@ pub fn background(dir: &Vec3f) -> Vec3f {
     let t = 0.5 * (dir[1] + 1.0);
     // linear interpolation based on t
     (1.0 - t) * Vec3f::repeat(1.0) + t * Vec3f::new(0.5, 0.7, 1.0)
-}
-
-pub fn cover_example_scene(aspect: f32) -> (Scene, Camera) {
-
-    let mut spheres: Vec<Sphere> = vec![];
-    let mut rng = thread_rng();
-    spheres.push(Sphere::fixed(vec3f!(0, -1000, 0), 1000.0, Box::new(Lambertian{albedo: vec3f!(0.7, 0.6, 0.5)})));
-    for a in -10..10 {
-        for b in -10..10 {
-            let mat: Box<dyn Material + Sync> = {
-                let choose_mat: f32 = random();
-
-                if choose_mat < 0.8 {
-                    Box::new(Lambertian {albedo: Vec3f::new(random(), random(), random())})
-                } else if choose_mat < 0.95 {
-                    Box::new(Metal {albedo: Vec3f::new(rng.gen(), rng.gen(), rng.gen()), fuzz: rng.gen_range(0.0, 0.5)})
-                } else {
-                    Box::new(Dielectric {refractive_index: 1.5})
-                }
-            };
-
-            let vel = {
-                if rng.gen_ratio(1, 4) {
-                    Some(Vec3f::new(rng.gen_range(0.0, 0.2), rng.gen_range(0.0, 0.5), rng.gen_range(0.0, 0.2)))
-                } else { None }
-            };
-
-            let radius = rng.gen_range(0.15, 0.25);
-            let center = Vec3f::new(a as f32 + 0.9 * rng.gen::<f32>(), radius, b as f32 + 0.9 * rng.gen::<f32>());
-
-            spheres.push(Sphere::new(center, radius, mat, vel));
-        }
-    }
-
-    spheres.push(Sphere::fixed(vec3f!(0, 1, 0), 1.0, Box::new(Dielectric{refractive_index: 1.5})));
-    spheres.push(Sphere::fixed(vec3f!(-4, 1, 0), 1.0, Box::new(Lambertian{albedo: vec3f!(0.3, 0.3, 0.8)})));
-    spheres.push(Sphere::fixed(vec3f!(4, 1, 0), 1.0, Box::new(Metal{albedo: vec3f!(0.7, 0.6, 0.5), fuzz: 0.01})));
-    let scene = Scene::new(spheres);
-
-    let lookfrom = vec3f!(13, 2, 3);
-    let lookat = vec3f!(0, 0, 0);
-    let lens = Lens {focus_dist: 10.0, aperture: 0.01};
-    let dist = 10.0;
-    let camera = Camera::new(lookfrom, lookat, vec3f!(0, 1, 0), 20f32.to_radians(), aspect, Some(lens), Some((0.0, 0.5)));
-
-    (scene, camera)
 }
