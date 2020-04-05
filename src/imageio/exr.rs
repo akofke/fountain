fe72617436e004a0264e9bf67492d8a48beeac48
Alex Kofke <akofke@gmail.com>
2020-04-05 12:15:21 -0400

    Switch to pure-rust exr library
    This makes it easier to build on windows and seems to successfully
    read some valid files that the bindings failed on.

--- src/imageio/exr.rs
+++ src/imageio/exr.rs
@@ -2,55 +2,86 @@ use std::path::Path;
 use crate::spectrum::Spectrum;
 use crate::Float;
 use std::fs::File;
-use openexr::{InputFile, FrameBufferMut, ScanlineOutputFile, Header, PixelType, FrameBuffer};
-use std::io::{Write, Seek};
+use std::io::{Write, Seek, BufReader};
+use exr::prelude::*;
+use exr::image::simple::{Image, Samples, Channel, Layer};
+use std::convert::TryInto;
+use smallvec::smallvec;
 
 pub fn read_exr(path: impl AsRef<Path>) -> anyhow::Result<(Vec<Spectrum>, (usize, usize))> {
-    let mut file = File::open(path)?; // TODO: BufReader
-    let mut input_file = InputFile::new(&mut file)?;
-    let (width, height) = input_file.header().data_dimensions();
-    let (ox, oy) = input_file.header().data_origin();
-    let mut pixels = vec![[0.0 as Float; 3]; (width * height) as usize];
+    let file = BufReader::new(File::open(path)?);
+    let image = Image::read_from_buffered(file, read_options::default()).unwrap();
 
-    {
-        let mut fb = FrameBufferMut::new_with_origin(ox, oy, width, height);
-        fb.insert_channels(
-            &[("R", 0.0), ("G", 0.0), ("B", 0.0)],
-            &mut pixels
-        );
-        input_file.read_pixels(&mut fb)?;
-    }
-    // TODO: could transmute without copying
-    let pixels = pixels.into_iter()
-        .map(|rgb| {
-            Spectrum::from(rgb)
-        })
-        .collect();
+    let layer = &image.layers[0];
+    let Vec2(w, h) = layer.data_size;
+    let r = Text::from("R").unwrap();
+    let b = Text::from("B").unwrap();
+    let g = Text::from("G").unwrap();
+    let r_chan = layer.channels.iter().find(|c| c.name == r).unwrap();
+    let b_chan = layer.channels.iter().find(|c| c.name == b).unwrap();
+    let g_chan = layer.channels.iter().find(|c| c.name == g).unwrap();
 
-    Ok((pixels, (width as usize, height as usize)))
+    let pixels: Vec<Spectrum> = match (&r_chan.samples, &g_chan.samples, &b_chan.samples) {
+        (Samples::F16(r), Samples::F16(g), Samples::F16(b)) => {
+            r.iter().zip(g.iter()).zip(b.iter())
+                .map(|((r, g), b)| {
+                    Spectrum::from([r.to_f32(), g.to_f32(), b.to_f32()])
+                })
+                .collect()
+        },
+        (Samples::F32(r), Samples::F32(g), Samples::F32(b)) => {
+            r.iter().zip(g.iter()).zip(b.iter())
+                .map(|((r, g), b)| {
+                    Spectrum::from([*r, *g, *b])
+                })
+                .collect()
+        }
+        _ => {
+            panic!()
+        }
+    };
+
+    Ok((pixels, (w as usize, h as usize)))
 }
 
 pub fn write_exr<W: Write + Seek>(writer: &mut W, img: Vec<Spectrum>, dims: (u32, u32)) -> anyhow::Result<()> {
     let (w, h) = dims;
-    let pixel_data: Vec<[Float; 3]> = img.into_iter()
-        .map(|s| s.into_array())
-        .collect();
+    let size = w * h;
+    let mut r_samples = Vec::with_capacity(size as usize);
+    let mut g_samples = Vec::with_capacity(size as usize);
+    let mut b_samples = Vec::with_capacity(size as usize);
 
-    let mut header = Header::new();
-    header
-        .set_resolution(w, h)
-        .add_channel("R", PixelType::FLOAT)
-        .add_channel("G", PixelType::FLOAT)
-        .add_channel("B", PixelType::FLOAT);
+    img.into_iter()
+        .for_each(|p| {
+            let [r, g, b] = p.into_array();
+            r_samples.push(r);
+            g_samples.push(g);
+            b_samples.push(b);
+        });
 
-    let mut output_file = ScanlineOutputFile::new(
-        writer,
-        &header
-    )?;
+    let r_chan = Channel::new_linear(
+        "R".try_into().unwrap(),
+        Samples::F32(r_samples),
+    );
+    let g_chan = Channel::new_linear(
+        "G".try_into().unwrap(),
+        Samples::F32(g_samples),
+    );
+    let b_chan = Channel::new_linear(
+        "B".try_into().unwrap(),
+        Samples::F32(b_samples),
+    );
 
-    let mut fb = FrameBuffer::new(w, h);
-    fb.insert_channels(&["R", "G", "B"], &pixel_data);
+    let layer = Layer::new(
+        "image".try_into().unwrap(),
+        Vec2(w as usize, h as usize),
+        smallvec![r_chan, g_chan, b_chan]
+    );
+    let layer = layer
+        .with_compression(Compression::RLE)
+        .with_block_format(None, LineOrder::Increasing);
 
-    output_file.write_pixels(&fb)?;
+    let image = Image::new_from_single_layer(layer);
+    image.write_to_buffered(writer, write_options::default()).unwrap();
     Ok(())
 }
